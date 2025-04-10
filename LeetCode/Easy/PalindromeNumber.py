f679b52b202355dcc26a5b18942a1e6b9beaaae0
Dheepthi-Reddy <144747236+Dheepthi-Reddy@users.noreply.github.com>
2025-04-10 06:03:11 -0500

    Update PalindromeNumber.py

--- LeetCode/Easy/PalindromeNumber.py
+++ LeetCode/Easy/PalindromeNumber.py
@@ -1,1 +1,28 @@
+'''
+PalindromeNumber:
 
+If a number is same as the initial number when arranged in reverse manner then its called a Palindrome number.
+
+example:
+
+343, 98789
+
+'''
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        
+        revNum = 0
+        actualNum = x # storing the initial number to compare
+        while x > 0:
+            lastDigit =  x % 10
+            revNum = (revNum * 10) + lastDigit # calculating revrse of the number
+            x = x // 10 
+        return actualNum == revNum
+
+'''
+
+121 - true
+-121 - false
+10 - false
+
+'''
