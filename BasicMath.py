0fdfae5f478df41803322acc6cd1c594f5c81c39
Dheepthi-Reddy <144747236+Dheepthi-Reddy@users.noreply.github.com>
2025-04-10 19:00:31 -0500

    Solved BasicMath.py

--- BasicMath.py
+++ BasicMath.py
@@ -1,1 +1,41 @@
+'''
 
+Digits are extracted in reverse order of the number
+
+'''
+
+# 1. Extracting Digits
+def extractedNumber(x):
+    exNum = 0
+    while x > 0:
+        lastDigit =  x % 10
+        exNum = (exNum * 10) + lastDigit
+        x = x // 10
+    print(exNum)
+
+extractedNumber(3654)
+
+'''
+O/P:
+
+4563
+
+'''
+
+# 2. Count of Digits
+def counter(x):
+    count = 0
+    while x > 0:
+        lastDigit =  x % 10
+        count += 1
+        x = x // 10
+    print(count)
+
+counter(8712345)
+
+'''
+O/P:
+
+7
+
+'''
